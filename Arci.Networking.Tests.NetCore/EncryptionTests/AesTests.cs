d64f9f08dc64ea368480f516936aa951a8b379d6
Arci <Arcidev@users.noreply.github.com>
2019-06-02 19:57:05 +0200

    Tests/NetCore: updated test package, simplified using statement

--- Arci.Networking.Tests.NetCore/EncryptionTests/AesTests.cs
+++ Arci.Networking.Tests.NetCore/EncryptionTests/AesTests.cs
@@ -44,73 +44,65 @@ namespace Arci.Networking.Tests.NetCore.EncryptionTests
         public void TestPacketEncryption()
         {
             var packet = new Packet(0).Builder().WriteBit(true).WriteBit(2).FlushBits().Write(new byte[] { 1, 2, 3 }).Build();
-            using (var aes = new AesEncryptor() { PaddingMode = PaddingMode.PKCS7 })
-            {
-                var decrypted = aes.Decrypt(aes.Encrypt(packet.Data));
-                Assert.Equal<byte>(packet.Data, decrypted);
-            }
+            using var aes = new AesEncryptor() { PaddingMode = PaddingMode.PKCS7 };
+            var decrypted = aes.Decrypt(aes.Encrypt(packet.Data));
+            Assert.Equal<byte>(packet.Data, decrypted);
         }
 
         [Fact]
         public void TestNonePadding()
         {
-            using (var aes = new AesEncryptor() { PaddingMode = PaddingMode.None })
-            {
-                var sb = new StringBuilder("Hello from unecrypted world");
-                if(sb.Length % aes.Key.Length != 0)
-                    sb.Append('\0', aes.Key.Length - sb.Length % aes.Key.Length);
+            using var aes = new AesEncryptor() { PaddingMode = PaddingMode.None };
+            var sb = new StringBuilder("Hello from unecrypted world");
+            if (sb.Length % aes.Key.Length != 0)
+                sb.Append('\0', aes.Key.Length - sb.Length % aes.Key.Length);
 
-                var value = sb.ToString();
-                var encryptedVal = aes.Encrypt(value);
-                Assert.NotEqual<byte>(Encoding.ASCII.GetBytes(value), encryptedVal);
+            var value = sb.ToString();
+            var encryptedVal = aes.Encrypt(value);
+            Assert.NotEqual<byte>(Encoding.ASCII.GetBytes(value), encryptedVal);
 
-                // No trim
-                var decryptedVal = aes.Decrypt(encryptedVal, Encoding.ASCII);
-                Assert.Equal(value, decryptedVal);
+            // No trim
+            var decryptedVal = aes.Decrypt(encryptedVal, Encoding.ASCII);
+            Assert.Equal(value, decryptedVal);
 
-                // Trim added zeroes
-                decryptedVal = aes.Decrypt(encryptedVal, Encoding.ASCII).TrimEnd('\0');
-                Assert.Equal(value.TrimEnd('\0'), decryptedVal);
-            }
+            // Trim added zeroes
+            decryptedVal = aes.Decrypt(encryptedVal, Encoding.ASCII).TrimEnd('\0');
+            Assert.Equal(value.TrimEnd('\0'), decryptedVal);
         }
 
         [Fact]
         public void TestKeyImmutability()
         {
-            using (var aes = new AesEncryptor(AesEncryptionType.Aes128Bits))
+            using var aes = new AesEncryptor(AesEncryptionType.Aes128Bits);
+            var key = aes.Key;
+            var iv = aes.IVec;
+
+            var keyList = key.ToList();
+            var ivList = iv.ToList();
+
+            for (var i = 0; i < (int)AesEncryptionType.Aes128Bits; i++)
             {
-                var key = aes.Key;
-                var iv = aes.IVec;
+                key[i] = (byte)((key[i] + 1) % byte.MaxValue);
+                iv[i] = (byte)((iv[i] + 1) % byte.MaxValue);
 
-                var keyList = key.ToList();
-                var ivList = iv.ToList();
+                Assert.NotEqual(keyList[i], key[i]);
+                Assert.Equal(keyList[i], aes.Key[i]);
 
-                for (var i = 0; i < (int)AesEncryptionType.Aes128Bits; i++)
-                {
-                    key[i] = (byte)((key[i] + 1) % byte.MaxValue);
-                    iv[i] = (byte)((iv[i] + 1) % byte.MaxValue);
-
-                    Assert.NotEqual(keyList[i], key[i]);
-                    Assert.Equal(keyList[i], aes.Key[i]);
-
-                    Assert.NotEqual(ivList[i], iv[i]);
-                    Assert.Equal(ivList[i], aes.IVec[i]);
-                }
+                Assert.NotEqual(ivList[i], iv[i]);
+                Assert.Equal(ivList[i], aes.IVec[i]);
             }
         }
 
         private void TestEncryption(PaddingMode padding)
         {
-            using (var aes = new AesEncryptor(AesEncryptionType.Aes256Bits) { PaddingMode = padding })
-            {
-                var value = "Hello from unecrypted world";
-                var encryptedVal = aes.Encrypt(value);
-                Assert.NotEqual<byte>(Encoding.ASCII.GetBytes(value), encryptedVal);
+            using var aes = new AesEncryptor(AesEncryptionType.Aes256Bits) { PaddingMode = padding };
+            var value = "Hello from unecrypted world";
+            var encryptedVal = aes.Encrypt(value);
+            Assert.NotEqual<byte>(Encoding.ASCII.GetBytes(value), encryptedVal);
 
-                // We need to trim \0 char from string as Aes ZeroesPadding is adding zeroes but not removing them
-                var decryptedVal = aes.Decrypt(encryptedVal, Encoding.ASCII).TrimEnd('\0');
-                Assert.Equal(value, decryptedVal);
-            }
+            // We need to trim \0 char from string as Aes ZeroesPadding is adding zeroes but not removing them
+            var decryptedVal = aes.Decrypt(encryptedVal, Encoding.ASCII).TrimEnd('\0');
+            Assert.Equal(value, decryptedVal);
         }
 
         private void TestAesEncryptorCreation(AesEncryptionType type)
@@ -124,14 +116,12 @@ namespace Arci.Networking.Tests.NetCore.EncryptionTests
                     iVec[i] = (byte)(byte.MaxValue - i);
             }
 
-            using (AesEncryptor aes = new AesEncryptor(type), aes2 = new AesEncryptor(aesKey, iVec))
-            {
-                Assert.NotNull(aes.Encryptors);
-                Assert.Equal(aes.Encryptors.Length, (int)type + iVec.Length);
+            using AesEncryptor aes = new AesEncryptor(type), aes2 = new AesEncryptor(aesKey, iVec);
+            Assert.NotNull(aes.Encryptors);
+            Assert.Equal(aes.Encryptors.Length, (int)type + iVec.Length);
 
-                Assert.NotNull(aes2.Encryptors);
-                Assert.Equal(aesKey.Concat(iVec), aes2.Encryptors);
-            }
+            Assert.NotNull(aes2.Encryptors);
+            Assert.Equal(aesKey.Concat(iVec), aes2.Encryptors);
         }
     }
 }
--- Arci.Networking.Tests.NetCore/EncryptionTests/RsaTests.cs
+++ Arci.Networking.Tests.NetCore/EncryptionTests/RsaTests.cs
@@ -21,21 +21,19 @@ namespace Arci.Networking.Tests.NetCore.EncryptionTests
 
         private void TestEncryption(bool useOaepPadding)
         {
-            using (RsaEncryptor rsa = new RsaEncryptor(RSAKey.Modulus, RSAKey.PublicExponent) { UseOAEPPadding = useOaepPadding },
-                rsa2 = new RsaEncryptor(RSAKey.RsaParams) { UseOAEPPadding = useOaepPadding })
-            {
-                var value = Encoding.ASCII.GetBytes("Hello from unecrypted world");
-                var rsaEncryptedValue = rsa.Encrypt(value);
-                var rsa2EncryptedValue = rsa2.Encrypt(value);
+            using RsaEncryptor rsa = new RsaEncryptor(RSAKey.Modulus, RSAKey.PublicExponent) { UseOAEPPadding = useOaepPadding },
+                rsa2 = new RsaEncryptor(RSAKey.RsaParams) { UseOAEPPadding = useOaepPadding };
+            var value = Encoding.ASCII.GetBytes("Hello from unecrypted world");
+            var rsaEncryptedValue = rsa.Encrypt(value);
+            var rsa2EncryptedValue = rsa2.Encrypt(value);
 
-                // Variable rsa is used only for encryption so we are not going to use it for decryption
-                // Instead We decrypt both values with rsa2 variable
-                var rsaDecryptedValue = rsa2.Decrypt(rsaEncryptedValue);
-                var rsa2DecryptedValue = rsa2.Decrypt(rsa2EncryptedValue);
+            // Variable rsa is used only for encryption so we are not going to use it for decryption
+            // Instead We decrypt both values with rsa2 variable
+            var rsaDecryptedValue = rsa2.Decrypt(rsaEncryptedValue);
+            var rsa2DecryptedValue = rsa2.Decrypt(rsa2EncryptedValue);
 
-                Assert.Equal<byte>(rsaDecryptedValue, rsa2DecryptedValue);
-                Assert.Equal<byte>(value, rsaDecryptedValue);
-            }
+            Assert.Equal<byte>(rsaDecryptedValue, rsa2DecryptedValue);
+            Assert.Equal<byte>(value, rsaDecryptedValue);
         }
     }
 }
